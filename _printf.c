a5aa40e8ad98b8c123cced2f67011293ee9d50ad
joejoturkson89 <turksonjoejo@gmail.com>
2023-08-22 14:46:14 -0700

    updated files

--- _printf.c
+++ _printf.c
@@ -1,58 +1,70 @@
 #include "main.h"
-
 /**
  * _printf - printf function
  * @format: format
- * Return: return printed characters
+ * Return: returns characters
  */
-int _printf(const char *format, ...) {
-  int char_to_print = 0;
-  va_list list;
+int _printf(const char *format, ...)
+{
+	int char_to_print = 0;
+	va_list list;
 
-  va_start(list, format);
-  while (*format) {
-    if (*format != '%') {
-      write(1, format, 1);
-      char_to_print++;
-    } else {
-      format++;
+	va_start(list, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_to_print++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+		}
+		switch (*format)
+		{
+			case 'c':
+				{
+					char c = va_arg(list, int);
+					write(1, &c, 1);
+					char_to_print++;
+					break;
+				}
+			case 's':
+				{
+					char *str = va_arg(list, char*);
+					int str_len = strlen(str);
+					write(1, str, str_len);
+					char_to_print++;
+					break;
+				}
+			case '%':
+				{
+					write(1, "%", 1);
+						char_to_print++;
+					break;
+				}
+			case 'd':
+			case 'i':
+				{
+					int num = va_arg(list, int);
+					char str[10];
+					sprintf(str, "%d", num);
+					write(1, str, strlen(str));
+					char_to_print += strlen(str);
+					break;
+				}
 
-      if (*format == '\0') {
-        return -1;
-      }
-
-      switch (*format) {
-        case 'c': {
-          char c = va_arg(list, int);
-          write(1, &c, 1);
-          char_to_print++;
-          break;
-        }
-        case 's': {
-          char *str = va_arg(list, char*);
-          int str_len = strlen(str);
-          write(1, str, str_len);
-          char_to_print += str_len;
-          break;
-        }
-
-case 'd':
-        case 'i': {
-          int num = va_arg(list, int);
-          char str[10];
-          itoa(num, str, 10);
-          write(1, str, strlen(str));
-          char_to_print += strlen(str);
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    }
-    format++;
-  }
-
-  va_end(list);
-  return char_to_print;
+			default:
+				{
+					break;
+				}
+		}
+		format++;
+	}
+	va_end(list);
+	return (char_to_print);
 }
+
--- main.h
+++ main.h
@@ -1,26 +1,15 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
 #include <stdarg.h>
-
 #include <unistd.h>
-
 #include <stdio.h>
-
 #include <limits.h>
-
 #include <string.h>
 
 
-
 int _printf(const char *format, ...);
-
 int putchar(int c);
-
 int binary(int num);
-
 int octal(int num);
-
 
 #endif
