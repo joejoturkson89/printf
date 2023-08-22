faa8c4562cc0685e48ece16aefe7b5641510511c
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-08-21 22:44:03 -0700

    updates-1

--- _printf.c
+++ _printf.c
@@ -1,71 +1,54 @@
-#include <stdio.h>
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
-*/
+ * _printf - printf function
+ * @format: format
+ * Return: return printed characters
+ */
+ 
 int _printf(const char *format, ...)
 {
-int i = 0;
-int printed = 0;
-int printed_chars = 0;
-int flags = 0;
-int width = 0;
-int precision = 0;
-int size = 0;
-int buff_ind = 0;
+int char_to_print = 0;
 va_list list;
-char buffer[BUFF_SIZE];
 
 if (format == NULL)
 return (-1);
 va_start(list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
+while (*format)
 {
-if (format[i] != '%')
+if (*format != '%')
 {
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
+write(1, format, 1);
+char_to_print++;
 }
 else
 {
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
+format++;
+}
+if (*format == '\0')
 return (-1);
-printed_chars += printed;
-}
-}
-
-print_buffer(buffer, &buff_ind);
-
-va_end(list);
-
-return (printed_chars);
-}
-
-/**
-* print_buffer - Print buffer
-* @buffer: Array
-* @buff_ind: Index
-*/
-void print_buffer(char buffer[], int *buff_ind)
+if (*format == 'c')
 {
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
+char c = va_arg(list, int);
+write(1, &c, strlen("char"));
+char_to_print++;
+}
+else if (*format == '%')
+{
+write(1, format, strlen(format));
+char_to_print++;
+}
+else if (*format == 's')
+{
+char str = va_arg(list, int);
+int str_len = 0;
+while (str[str_len] != '\0')
+str_len++;
+write(1, str, str_len);
+char_to_print += str_len;
+}
+format++;
+}
+va_end(list);
+return (char_to_print);
 }
